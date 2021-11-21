990617aa251eb2f11a9d529d6a22275fa5efca8b
cybaol <cybao292261@163.com>
2021-11-21 19:40:27 +0800

    fixed prompt missing

--- env.zsh
+++ env.zsh
@@ -9,4 +9,11 @@ export TERM_ITALICS=true
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 
-export GOPROXY=https://mirrors.aliyun.com/goproxy/
+export GOPROXY=https://goproxy.io,direct
+
+# ROS
+source /opt/ros/melodic/setup.zsh
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/opt/ros/melodic/include
+
+# libopencm3
+# export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/arm-none-eabi/include/
